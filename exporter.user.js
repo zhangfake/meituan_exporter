e7ba0e4880450f25272e895fd380abf047ea09a7
zhangfake <109203376+zhangfake@users.noreply.github.com>
2023-12-08 10:11:41 +0800

    Update exporter.user.js

--- exporter.user.js
+++ exporter.user.js
@@ -84,21 +84,55 @@ layui.use(function () {
       layer.msg('没有数据可以导出', { icon: 1 });
       return;
     }
-    let csv = products.map(item => {
-      return {
-        "美团ID": item.id || '',
-        "美团名称": item.name || '',
-        "美团类目": item.categoryNamePath || '',
-        "美团商品原始数据": JSON.stringify(item)
-      }
-    })
-
-    const workbook = XLSX.utils.book_new();
-    const worksheet = XLSX.utils.json_to_sheet(csv, { header: ["美团ID", "美团名称", "美团类目", "美团商品原始数据"] });
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-    // 将工作簿保存为 Excel 文件
-    XLSX.writeFile(workbook, "data.xlsx");
+    let csv = [["美团ID", "美团名称", "美团类目", "美团商品原始数据"]].concat(products.map(item => {
+      return [
+        item.id || '',
+        item.name || '',
+        item.categoryNamePath || '',
+        JSON.stringify(item)
+      ]
+    }))
+    exportToCsv('data.csv', csv)
     layer.closeAll('loading')
+  }
+  function exportToCsv(filename, rows) {
+    var processRow = function (row) {
+      var finalVal = '';
+      for (var j = 0; j < row.length; j++) {
+        var innerValue = row[j] === null ? '' : row[j].toString();
+        if (row[j] instanceof Date) {
+          innerValue = row[j].toLocaleString();
+        };
+        var result = innerValue.replace(/"/g, '""');
+        if (result.search(/("|,|\n)/g) >= 0)
+          result = '"' + result + '"';
+        if (j > 0)
+          finalVal += ',';
+        finalVal += result;
+      }
+      return finalVal + '\n';
+    };
+  
+    var csvFile = '';
+    for (var i = 0; i < rows.length; i++) {
+      csvFile += processRow(rows[i]);
+    }
+  
+    var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) {
+      navigator.msSaveBlob(blob, filename);
+    } else {
+      var link = document.createElement("a");
+      if (link.download !== undefined) {
+        var url = URL.createObjectURL(blob);
+        link.setAttribute("href", url);
+        link.setAttribute("download", filename);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    }
   }
 
   const button = document.createElement('div')
